a37d3a64db70be4b2f4141f51d61e5b30f76ee03
Omadoye Jedidiah <omadoyejed@gmail.com>
2017-02-21 11:12:06 +0100

    Working on auth

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+(function(){
+// Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyD9XNmswgLSwsJoCbM89meKN_offp-ERdE",
+    authDomain: "ideabox-15cf7.firebaseapp.com",
+    databaseURL: "https://ideabox-15cf7.firebaseio.com",
+    storageBucket: "ideabox-15cf7.appspot.com",
+    messagingSenderId: "883050155915"
+    };
+    firebase.initializeApp(config);
+  }());
